f747125ca849c594016027ec291330849858f8a1
David Henry <dw_henry@yahoo.com.au>
2015-05-02 19:43:24 +0100

    Start to fill out the menu items

--- app/site/app/controllers/site/application_controller.rb
+++ app/site/app/controllers/site/application_controller.rb
@@ -1,5 +1,10 @@
 module Site
   class ApplicationController < ActionController::Base
     helper 'site/events'
+    helper_method :current_user
+
+    def current_user
+      nil # TODO: add user login and credentials
+    end
   end
 end
