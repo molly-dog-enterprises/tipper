bf4d15d43796fae8543945c990af7e33cad67175
David Henry <dw_henry@yahoo.com.au>
2015-07-08 08:34:40 +0100

    Fix param passing to slider

--- app/site/app/assets/javascripts/site/components/components/pickSlider.js.jsx
+++ app/site/app/assets/javascripts/site/components/components/pickSlider.js.jsx
@@ -16,7 +16,7 @@ MDE.Constants.PickSlider = (function() {
       return (
         <div className='slider'>
           {this.props.row.pick_name} by {this.state.by}
-          <ReactSlider defaultValue={scores} className='horizontal-slider' min={-50} max={50} snapDragDisabled='true' />
+          <ReactSlider defaultValue={scores} className='horizontal-slider' min={-50} max={50} snapDragDisabled={true} />
           <div className='team-container'>
             <div className='align-left'>{this.props.row.home} {this.props.row.home_score}</div>
             <div className='align-right'>{this.props.row.away_score} {this.props.row.away}</div>
